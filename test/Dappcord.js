2c9c11038007c1d8e2961c3b47cbfa7f09582d69
Harsh <harshb15003@gmail.com>
2024-02-10 02:08:17 +0530

    added tests

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,27 +1,53 @@
 const {expect} = require('chai');
+const { ethers } = require('hardhat');
+
 
 const tokens =(n)=>{
-  return ethers.parseUnits(n.toString(),'ether');
+  return ethers.utils.parseEther(n.toString(),'ether');
 }
 
 describe("Dappcord",function(){
   let dappcord;
+  let deployer,user;
+  const NAME = "Dappcord";
+  const SYMBOL = "DC";
   beforeEach(async()=>{
+    [deployer,user] = await ethers.getSigners();
     const Dappcord = await ethers.getContractFactory("Dappcord");
-    dappcord = await Dappcord.deploy("Dappcord","DC");
-  
+    dappcord = await Dappcord.deploy(NAME,SYMBOL);
+
+    const transaction = await dappcord.connect(deployer).createChannel("general",tokens(1))
+    await transaction.wait();
   })
   describe("Deployment",function(){
+    
     it("Sets the name",async()=>{
       let result = await dappcord.name();
-      expect(result).to.equal("Dappcord");
+      expect(result).to.equal(NAME);
+    })
+  
+    it("Sets the symbol",async()=>{ 
+      result = await dappcord.symbol();
+      expect(result).to.equal(SYMBOL);
+    })
+
+    it("Sets the owner",async()=>{
+      const result = await dappcord.owner();
+      expect(result).to.equal(deployer.address);
     })
   })
 
-  describe("Deployment",function(){
-    it("Sets the symbol",async()=>{ 
-      result = await dappcord.symbol();
-      expect(result).to.equal("DC");
+  describe("Creating Channels",()=>{
+    it("Returns total channels",async()=>{
+      const result = await dappcord.totalChannels();
+      expect(result).to.be.equal(1);
+    })
+
+    it("Returns channel attributes",async()=>{
+      const channel = await dappcord.getChannel(1);
+      expect(channel.id).to.be.equal(1);
+      expect(channel.name).to.be.equal('general');
+      expect(channel.cost).to.be.equal(tokens(1));
     })
   })
 })
